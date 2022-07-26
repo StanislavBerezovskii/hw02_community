493a1f7b375ad6f34144581bfcede2d54462f007
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-26 16:29:05 +0200

    version 1.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,27 +5,19 @@ from .models import Post, Group
 
 
 def index(request):
-    title = 'Последние обновления на сайте'
-    text = 'Последние обновления на сайте'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     context = {
-        'title': title,
-        'text': text,
-        'posts': posts,
+        'posts': posts
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    title = f'Записи сообщества {group.title}'
-    text = f'Записи сообщества {group.title}'
+    posts = group.posts.all()[:10]
     context = {
         'group': group,
         'posts': posts,
-        'title': title,
-        'text': text,
     }
     return render(request, 'posts/group_list.html', context)
 
