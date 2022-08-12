49fb63ab92647d30b1a4f21b92fa87cd6468f8d5
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-12 10:30:12 +0200

    put some gloss on yatube/urls.py

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -11,23 +11,27 @@ app_name = 'users'
 urlpatterns = [
     path(
         'logout/',
-        LogoutView.as_view(template_name='users/logged_out.html'),
+        LogoutView.as_view(
+            template_name='users/logged_out.html'),
         name='logout'
     ),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'),
+        LoginView.as_view(
+            template_name='users/login.html'),
         name='login'
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change.html'),
+        PasswordChangeView.as_view(
+            template_name='users/password_change.html'),
         name='password_change'
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
         name='password_change_done'
     )
 ]
