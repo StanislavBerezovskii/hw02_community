6dd8557c33d95a6c5e73eda7f75d69463d324587
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-20 13:15:46 +0200

    latest

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,10 @@
 # posts/views.py
 from django.shortcuts import render, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
 from .models import Post, Group
 
 
+@login_required
 def index(request):
     posts = Post.objects.all()[:10]
     context = {
