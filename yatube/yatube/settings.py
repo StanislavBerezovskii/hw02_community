132762b741b211b332a476ee3fac56eaea808f05
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-20 22:19:40 +0200

    added conetxt processor year. for real

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'core.context_processors.welcome.welcome',
-
+                'core.context_processors.year.year',
             ],
         },
     },
