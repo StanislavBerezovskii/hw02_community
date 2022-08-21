867989f5366af2f1b37929b5c015c4192ba02f5b
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-21 22:12:36 +0200

    added static pages baed on TemplatesView

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'about.apps.AboutConfig',
 ]
 
 MIDDLEWARE = [
