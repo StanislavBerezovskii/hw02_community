914bfd22feb10249ff3290f2f64cd784b00173ba
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-21 15:01:26 +0200

    added paginator support for group_list

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -19,10 +19,13 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    post_list = group.posts.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
 
