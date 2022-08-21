4a21a8650ad5bf4e438886fb477f2941d9fc1db7
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-21 23:44:32 +0200

    added profile page

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('base_template/', views.show_base_template, name='show_base'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Post, Group
+from .models import Post, Group, User
 
 
 @login_required
@@ -33,3 +33,26 @@ def group_posts(request, slug):
 def show_base_template(request):
     template = 'base.html'
     return render(request, template)
+
+
+@login_required
+def profile(request, username):
+    post_author = get_object_or_404(User, username=username)
+    post_list = post_author.posts.all().order_by('-pub_date')
+    post_count = post_author.posts.count()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'username': username,
+        'post_count': post_count,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    context = {
+    }
+    return render(request, 'posts/post_detail.html', context)
