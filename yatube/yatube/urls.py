35ddcb4a06992cf44a2391fffab3cf0d9f472529
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-21 22:22:08 +0200

    really added about app

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('about/', include('about.urls.py', namespace='about')),
+    path('about/', include('about.urls', namespace='about')),
 ]
