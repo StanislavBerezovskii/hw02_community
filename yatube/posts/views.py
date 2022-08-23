7e2cb8c26598449f7a6717c9eef00de9e77fd8bc
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-08-23 18:45:03 +0200

    beta version post_create + post_edit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -85,7 +85,8 @@ def post_edit(request, post_id):
         return redirect('posts:post_detail', post_id)
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
-        post = form.save()
+        post = form.save(commit=False)
+        post.save()
         return redirect('posts:post_detail', post_id)
     context = {
         'form': form,
